cc34d181f4aa8903dcb3274b8d8c7a149ce76015
gmc bjorjka <117149089+gmcbjorjka@users.noreply.github.com>
2023-10-10 21:02:33 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const ucapanButton = document.getElementById("ucapanButton");
+
+  ucapanButton.addEventListener("click", function () {
+      // Mengarahkan pengguna ke halaman selanjutnya
+      window.location.href = "halaman_selanjutnya.html";
+  });
+});
